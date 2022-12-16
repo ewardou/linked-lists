6b06a40e2949e30ccbfcbdf4d5732cf71a8287b7
Eduardo Sánchez <eduardosr1599@gmail.com>
2022-12-16 15:45:49 -0400

    Add more methods to the linked list object

--- list.js
+++ list.js
@@ -7,22 +7,24 @@ class Node{
 }
 
 class LinkedList{
-    constructor(value=null,nextNode=null){
-        this.head=value
-        this.next=nextNode
-    }
+    // constructor(value=null,nextNode=null){
+    //     this.head=value
+    //     this.next=nextNode
+    // }
 
     prepend(value){
         const newNode=new Node(value);
-        if (this.head){
-            const oldNode=new Node(this.head,this.next)
+        if (this.value || this.value===0){
+            const oldNode=new Node(this.value,this.next)
             this.next=oldNode;
+        } else {
+            this.next=null
         }
-        this.head=newNode.value;
+        this.value=newNode.value;
     }
 
     append(value){
-        if (this.head===null){
+        if (!this.value && this.value!==0){
             return this.prepend(value);
         }
         let currentNode=this;
@@ -33,7 +35,7 @@ class LinkedList{
     }
 
     size(){
-        if (this.head===null){
+        if (!this.value && this.value!==0){
             return 0
         }
         let count=1;
@@ -44,4 +46,73 @@ class LinkedList{
         }
         return count;
     }
+
+    head(){
+        return new Node(this.value,this.next);
+    }
+
+    tail(){
+        let currentNode=this;
+        while(currentNode.next!==null){
+            currentNode=currentNode.next;
+        }
+        return currentNode;
+    }
+
+    at(index){
+        if (index<0){
+            throw new Error("Index cannot be less than 0");
+        }
+        if (index>this.size()-1){
+            throw new Error("Requested index is higher than list size");
+        }
+        let currentNode=this;
+        for (let i=0; i<index;i++){
+            currentNode=currentNode.next;
+        }
+        return currentNode;
+    }
+
+    pop(){
+        if (this.size()===1){
+            delete this.value;
+            delete this.next;
+            return 
+        }
+        const lastIndex=this.size()-1;
+        const previousNode = this.at(lastIndex-1);
+        previousNode.next=null;
+    }
+
+    contains(value){
+        let currentNode=this;
+        while (currentNode.next!==null){
+            currentNode=currentNode.next;
+            if (currentNode.value===value){
+                return true;
+            }
+        }
+        return this.value===value;
+    }
+
+    find(value){
+        if (this.contains(value)===false){
+            return null;
+        }
+        let index=0;
+        let currentValue=this.value;
+        let currentNode=this;
+        while(currentValue!==value){
+            currentNode=currentNode.next;
+            currentValue=currentNode.value;
+            index+=1;
+        }
+        return index;
+    }
 }
+
+let testList= new LinkedList();
+testList.append(0);
+testList.append(1);
+testList.append(2);
+testList.append(3);
